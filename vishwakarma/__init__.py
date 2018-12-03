3c3dd73118f6b7e040ae2b53ac0a32fbec687759
aghanti7 <ajayghanti@gmail.com>
2018-12-03 22:31:15 +0530

    minor version upgrade

--- vishwakarma/__init__.py
+++ vishwakarma/__init__.py
@@ -1,2 +1,17 @@
-'''Version of the vishwakarma package'''
-__version__ = '0.0.1'
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    no description available
+
+    :copyright: (c) 2018 by Diagram AI.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'vishwakarma'
+__version__ = '0.0.2'
+__author__ = 'diagram-ai'
+
+from .pgmplot import pgmplot
