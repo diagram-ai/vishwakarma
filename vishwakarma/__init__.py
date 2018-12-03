39abbdf822fe7591dc98a3ad1f795798f6429b18
aghanti7 <ajayghanti@gmail.com>
2018-12-03 22:36:47 +0530

    update desc

--- vishwakarma/__init__.py
+++ vishwakarma/__init__.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""
-    __init__.py
-    ~~~~~~~~~~~
-
-    no description available
+'''
+    Python visualization library for Probabilistic Graphical Models, and a lot more!
 
     :copyright: (c) 2018 by Diagram AI.
     :license: see LICENSE for more details.
-"""
+'''
 
 __title__ = 'vishwakarma'
 __version__ = '0.0.2'
