737efcda3019da653b142eb8ce14647edaf3e898
nikhilskashyap <nikhilskashyap@nikhilskashyaps-MacBook-Air.local>
2020-01-04 09:49:03 +0530

    ec2

--- vishwakarma/pgmplot.py
+++ vishwakarma/pgmplot.py
@@ -55,8 +55,8 @@ def pgmplot(obj, width=600):
             # serialize the object
             data = pickle.dumps(obj)
 
-            url = 'http://api.diagram.ai/vishwakarma/pgmplot/'
-
+            _url = 'http://ec2-3-136-87-27.us-east-2.compute.amazonaws.com/vishwakarma/'
+            
             resp = requests.post(url,data=data,
                     headers={'Content-Type': 'application/octet-stream'})
 
